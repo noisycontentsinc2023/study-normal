e5e7ce27deb79511cb94855f1aca3547c87244aa
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-17 21:26:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -139,8 +139,8 @@ async def event(ctx):
         bs=BeautifulSoup(url_open,'html.parser')
 
         #이벤트 이미지 가져오기
-        vent1 = bs.select('div>h2>a')[0]
-        vent11 = bs.select('div>h2>a')[1]
+        vent1 = bs.select('div>h2>a')[0].get_text()
+        vent11 = bs.select('div>h2>a')[1].get_text()
 
         embed = discord.Embed(title="현재 진행중인 이벤트", description="자세한 정보는 홈페이지를 참고해주세요 http://studymini.com/", color=0x62c1cc)
         embed.add_field(name="",value=f"1:{vent1}, {vent11}, inline=false")
