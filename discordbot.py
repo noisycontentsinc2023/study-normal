894998f948c08f73e7a6666dad4bbfa4dde0d97d
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-17 14:28:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,7 @@ import os
 import random
 import googletrans 
 from discord import Embed
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-
-def set_chrome_driver():
-    chrome_options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    return driver
 
 translator = googletrans.Translator()
 intents = discord.Intents.default()
@@ -115,37 +107,9 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         
 #------------------------------------------------검색------------------------------------------------------# 
-@bot.event
-async def on_message(message):
-    if message.content.startswith('!유튜브'):
-        Text = ""
-        learn = message.content.split(" ")
-        vrsize = len(learn)  # 배열크기
-        vrsize = int(vrsize)
-        for i in range(1, vrsize):  # 띄어쓰기 한 텍스트들 인식함
-            Text = Text + " " + learn[i]
-        encText = Text
 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        driver.get('https://www.youtube.com/results?search_query='+encText) #유튜브 검색링크
-        source = driver.page_source
-        bs = bs4.BeautifulSoup(source, 'lxml')
-        entire = bs.find_all('a', {'id': 'video-title'}) # a태그에서 video title 이라는 id를 찾음
-
-        embed = discord.Embed(
-            title="영상들!",
-            description="검색한 영상 결과",
-            colour=discord.Color.blue())
-
-        for i in range(0, 4):
-            entireNum = entire[i]
-            entireText = entireNum.text.strip()  # 영상제목
-            print(entireText)
-            test1 = entireNum.get('href')  # 하이퍼링크
-            print(test1)
-            rink = 'https://www.youtube.com'+test1
-            embed.add_field(name=str(i+1)+'번째 영상',value=entireText + '\n링크 : '+rink)
-        await message.channel.send(embed=embed)
 
 #Run the bot
 bot.run(TOKEN)
+    
+
