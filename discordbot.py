f49fbff6a1bc34231eaa4d998bd2c5df146aa2cb
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-18 00:17:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -154,10 +154,12 @@ async def event(ctx):
 
         embed = discord.Embed(title="현재 진행중인 이벤트", description="자세한 정보는 홈페이지를 참고해주세요 http://studymini.com/", color=0x62c1cc)
         embed.add_field(name="이벤트 1", value=f"{vent1}\n\u200c{vent11}\n[자세히 보기]({vent11_url})", inline=True)
-        embed.add_field(name="\u200b", value="\u200b", inline=True)  # 빈 필드 추가
         embed.add_field(name="이벤트 2", value=f"{vent2}\n\u200c{vent21}\n [자세히 보기]({vent21_url})", inline=True)
-        embed.add_field(name="\u200b", value="\u200b", inline=True)  # 빈 필드 추가
         embed.add_field(name="이벤트 3", value=f"{vent3}\n\u200c{vent31}\n [자세히 보기]({vent31_url})", inline=True)
+
+        # 가운데 정렬
+        for field in embed.fields:
+            field.value = f"{field.value.center(40, ' ')}"
             
         await ctx.send(embed=embed)
 
