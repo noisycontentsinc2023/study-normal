1d648fbfd91a6f10d082e2ac4806ec6ba12f7e10
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-17 23:03:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -149,9 +149,9 @@ async def event(ctx):
         vent31 = bs.select('div>h2>a')[7].get_text()
 
         embed = discord.Embed(title="현재 진행중인 이벤트", description="자세한 정보는 홈페이지를 참고해주세요 http://studymini.com/", color=0x62c1cc)
-        embed.add_field(name="이벤트 1", value=f"{vent1}\n, {vent11}")
-        embed.add_field(name="이벤트 2", value=f"{vent2}\n, {vent21}")
-        embed.add_field(name="이벤트 3", value=f"{vent3}\n, {vent31}")
+        embed.add_field(name="이벤트 1", value=f"{vent1}\n {vent11}")
+        embed.add_field(name="이벤트 2", value=f"{vent2}\n {vent21}")
+        embed.add_field(name="이벤트 3", value=f"{vent3}\n {vent31}")
             
         await ctx.send(embed=embed)
 
