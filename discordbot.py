46c293d7faa191835233ae386c48813ca154e7eb
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-20 18:12:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -209,49 +209,52 @@ async def search(ctx, *args):
 
 #------------------------------------------------투표------------------------------------------------------#  
 @bot.command()
-async def 투표(ctx, title, *choice):
+async def표투표(ctx, *, args):
     '''
-    투표
-    :param title: 투표 제목
-    :param choice: 선택지 (최대 9개)
+    vote
+    :param args: title and choice separated by commas (",")
     '''
-    # TODO 웹
-    # TODO 중복투표 불가능하게
-    # TODO 익명투표 만들기
-    # 투표 도움말
-    if title is None and choice == ():
-        embed = discord.Embed(title=f'투표 도움말', description=f'개발자: gagip')
-        embed.add_field(name=f'좋아요/싫어요', value=f'!투표 제목')
-        embed.add_field(name=f'복수응답(1-9)', value=f'!투표 제목 내용1 내용2 ...')
+    # TODO web
+    # Disable TODO duplicate voting
+    # Create a TODO anonymous vote
+    # Voting help
+    if not args:
+        embed = discord.Embed(title=f'Vote Help', description=f'Developer: gagip')
+        embed.add_field(name=f'Like/Dislike', value=f'!vote title')
+        embed.add_field(name=f'multiple responses (1-9)', value=f'!vote title, choice 1, choice 2, ..., choice 9')
         await ctx.send(embed=embed)
-    
-    # 투표 진행
     else:
+        # Split title and options
+        parts = [part.strip() for part in args.split(',')]
+        title = parts[0]
+        options = parts[1:]
+
+        # Create embed
         embed = discord.Embed(title=title)
-        if choice == ():
-            # 좋아요/싫어요
+        if not options:
+            # Like/Dislike
             message = await ctx.send(embed=embed)
             await message.add_reaction('👍')
             await message.add_reaction('👎')
         else:
-            # 복수응답(1-10)
-            emoji_list = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣']  # 선택지 번호 라벨
+            # Multiple responses (1-9)
+            emoji_list = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'] # Option number label
 
             s = ''
             emoji = iter(emoji_list)
-            for cont in choice:
+            for option in options:
                 try:
-                    s += f'{next(emoji)} {cont}\n'
-                except ValueError:
-                    await ctx.sent('투표 선택지는 9개까지만 가능합니다.')
+                    s += f'{next(emoji)} {option}\n'
+                except StopIteration:
+                    await ctx.send('You can only vote up to 9 options.')
                     return
 
-            # 디스코드에 제목 출력
-            embed.add_field(name=s, value='1은 기본적으로 있음, 중복투표 가능')
+            # Output title to Discord
+            embed.add_field(name=s, value='1 is present by default, duplicate voting possible')
             message = await ctx.send(embed=embed)
 
-            # 디스코드에 선택지 출력
-            for i in range(len(choice)):
+            # Output options to Discord
+            for i in range(len(options)):
                 await message.add_reaction(emoji_list[i])
                 
 #Run the bot
