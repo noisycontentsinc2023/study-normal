9c795f5fc69e8fbe5e7e53c6fb0aff046bc9c79a
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-20 19:05:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,8 @@ import requests
 import re
 import time
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 from bs4 import BeautifulSoup
 from discord import Embed
@@ -208,6 +210,8 @@ async def search(ctx, *args):
     await ctx.send('에러가 발생했어요! 명령어를 깜빡 하신건 아닐까요?')
 
 #------------------------------------------------투표------------------------------------------------------#  
+vote_counts = {}  # Create an empty dictionary to store vote counts
+
 @bot.command()
 async def 투표(ctx, *, args):
     '''
@@ -245,8 +249,9 @@ async def 투표(ctx, *, args):
             for option in options:
                 try:
                     s += f'{next(emoji)} {option}\n'
+                    vote_counts[option] = 0  # Initialize vote count for each option to 0
                 except StopIteration:
-                    await ctx.send('옵션은 9개까지만 가능해')
+                    await ctx.send('옵션은 9개까지만 가능요')
                     return
 
             # Output title to Discord
@@ -256,6 +261,33 @@ async def 투표(ctx, *, args):
             # Output options to Discord
             for i in range(len(options)):
                 await message.add_reaction(emoji_list[i])
+
+            # Wait for reactions
+            await asyncio.sleep(60)
+
+            # Get updated message with reactions
+            message = await ctx.channel.fetch_message(message.id)
+
+            # Update vote counts based on reactions
+            for reaction in message.reactions:
+                if reaction.emoji in emoji_list:
+                    index = emoji_list.index(reaction.emoji)
+                    option = options[index]
+                    count = reaction.count - 1  # Subtract 1 to exclude the bot's own reaction
+                    vote_counts[option] += count
+
+            # Create bar chart of vote counts
+            fig, ax = plt.subplots()
+            options = list(vote_counts.keys())
+            counts = list(vote_counts.values())
+            y_pos = np.arange(len(options))
+            ax.barh(y_pos, counts)
+            ax.set_yticks(y_pos)
+            ax.set_yticklabels(options)
+            ax.invert_yaxis()  # Invert y-axis to display options from top to bottom
+            ax.set_xlabel('Vote Count')
+            ax.set_title(title)
+            plt.savefig('vote_counts.png')
                 
 #Run the bot
 bot.run(TOKEN)
