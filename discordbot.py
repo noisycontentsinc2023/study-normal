df310c8f14f88643a85df100dbed9191b6b2f82a
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-21 15:18:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -300,6 +300,20 @@ async def close_poll(ctx, poll_id: str):
     # Get poll message
     poll_message_id = poll_data['message_id']
     poll_message = await ctx.channel.fetch_message(poll_message_id)
+    
+    # Get poll results
+    poll_results = {}
+    for reaction in poll_message.reactions:
+        emoji = get_emoji(reaction.emoji)
+        if emoji in poll_data['options']:
+            async for user in reaction.users():
+                if user != bot.user:
+                    if emoji in poll_results:
+                        poll_results[emoji].append(user.id)
+                else:
+                    poll_results[emoji] = [user.id]
+                    poll_data['votes'][emoji] = []  # initialize empty list for option
+                poll_data['votes'][emoji].append(user.id)  # add user ID to option list
 
     # Get poll results
     poll_results = {}
