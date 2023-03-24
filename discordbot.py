99e03e8a51e7f312eed653982d2d1e310d88d77f
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-24 16:43:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -452,6 +452,10 @@ async def speak(ctx):
     await display_speak(ctx)
     
 async def display_speak(ctx):
+    if user_mentions_instance is None:
+        await ctx.send("User mentions not loaded yet. Please wait a moment and try again.")
+        return
+
     user_mentions = user_mentions_instance.user_mentions
     view = CustomView(user_mentions)
     buttons = [
