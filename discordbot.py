6fa91ccf12588cfa4cf28d0834f2fec2c2af330c
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-04 17:43:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,9 @@ import uuid
 import string
 import aiofiles
 import json
+import gspread
 
+from google.oauth2 import service_account
 from bs4 import BeautifulSoup
 from discord import Embed
 from discord.ext import tasks
@@ -522,6 +524,118 @@ async def study(ctx):
         message = "오늘 공부는 패스!"
     embed = discord.Embed(title="공부..할까..말까?", description=message, color=0xffd700)
     await ctx.send(embed=embed)
+    
+#-------------------------메모모-------------------------#
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds_info = {
+  "type": "service_account",
+  "project_id": "thematic-bounty-382700",
+  "private_key_id": "502d8dd4f035d15b57bff64ee323d544d28aedc4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "noisycontents@thematic-bounty-382700.iam.gserviceaccount.com",
+  "client_id": "107322055541690533468",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com"
+}
+creds = service_account.Credentials.from_service_account_info(info=creds_info, scopes=scope)
+client = gspread.authorize(creds)
+sheet = client.open('테스트').worksheet('메모')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@bot.command(name='메모')
+async def memo(ctx):
+    # Extract user ID and memo content
+    user_id = str(ctx.author.id)
+    message_content = ctx.message.content
+    memo = message_content.split('!메모 ')[1]
+
+    # Find the next available column to write data to
+    header_values = sheet.row_values(1)
+    if not header_values:
+        col = 1
+        sheet.update_cell(1, col, user_id)
+    elif user_id not in header_values:
+        last_user_col = sheet.col_count
+        last_user_id = header_values[-1]
+        last_user_id_col = header_values.index(last_user_id) + 1
+        col = last_user_id_col + 1
+        sheet.add_cols(1)
+        sheet.update_cell(1, col, user_id)
+    else:
+        col = header_values.index(user_id) + 1
+
+    # Find the next available row to write data to
+    values = sheet.col_values(col)
+    row = len(values) + 1
+
+    # Write user ID and memo content to the corresponding cell
+    if row == 1:
+        sheet.update_cell(row, col, user_id)
+        row += 1
+    sheet.update_cell(row, col, memo)
+
+    await ctx.send(f'{ctx.author.mention} 메모가 저장됐어요')
+    
+@bot.command(name='메모보기')
+async def view_memo(ctx):
+    # Extract user ID
+    user_id = str(ctx.author.id)
+
+    # Find the column index of the user ID in row 1
+    header_values = sheet.row_values(1)
+    try:
+        col = header_values.index(user_id) + 1
+    except ValueError:
+        await ctx.send(f'{ctx.author.mention} 메모를 찾을 수 없습니다 :(')
+        return
+
+    # Retrieve memo content for the user from row 2
+    memo_values = sheet.col_values(col)[1:]
+    if memo_values:
+        memo_list = [f'{i+1}. {memo}' for i, memo in enumerate(memo_values)]
+        memo_str = '\n'.join(memo_list)
+        embed = discord.Embed(title=f'{ctx.author.name}'의 메모입니다, description=memo_str)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send(f'{ctx.author.mention} 메모를 찾지 못했어요')
+        
+@bot.command(name='메모삭제')
+async def delete_memo(ctx, memo_number: int):
+    # Extract user ID
+    user_id = str(ctx.author.id)
+
+    # Find the column index of the user ID in row 1
+    header_values = sheet.row_values(1)
+    try:
+        col = header_values.index(user_id) + 1
+    except ValueError:
+        await ctx.send(f'{ctx.author.mention} 메모를 찾지 못했어요')
+        return
+
+    # Retrieve memo content for the user from row 2
+    memo_values = sheet.col_values(col)[1:]
+
+    # Check if the given memo number is valid
+    if memo_number <= 0 or memo_number > len(memo_values):
+        await ctx.send(f'{ctx.author.mention} 메모 번호가 틀린 것 같아요')
+        return
+
+    # Delete the memo content from the spreadsheet and shift the remaining memos up
+    index_to_delete = memo_number + 1
+    remaining_memos = memo_values[index_to_delete - 2:]
+    for i, _ in enumerate(remaining_memos[:-1]):
+        sheet.update_cell(index_to_delete + i, col, remaining_memos[i + 1])
+
+    # Clear the last cell after shifting the memos or if the deleted memo is the last one
+    sheet.update_cell(index_to_delete + len(remaining_memos) - 1, col, '')
+
+    await ctx.send(f'{ctx.author.mention} {memo_number} 번 ')
     
 #Run the bot
 bot.run(TOKEN)
