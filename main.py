f016fcc8c0e781db367dc73339b0290902a0a1fa
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 17:55:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from basic import easic
 from event1 import event1
 
+bot.add_cog(basic(bot))
+bot.add_cog(event1(bot))
+
 if __name__ == '__main__':
-    bot.add_cog(basic(bot))
-    bot.add_cog(event1(bot))
     bot.run(TOKEN)
