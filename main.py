e671ec07b2225feb2be5ea8f5870b4563a3392cd
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 18:32:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from discord.ext import commands
 from basic import easic
-from huodong import huodong
 
 bot.add_cog(basic(bot))
-bot.add_cog(huodong(bot))
 
 if __name__ == '__main__':
     bot.run(TOKEN)
