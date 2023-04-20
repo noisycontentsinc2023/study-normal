19675e22f80d95cdf074a5c3fea35e8953d3e86f
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 10:46:06 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 from discord.ext import commands
 
-PREFIX = os.environ['PREFIX']
-prefix = !
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 def get_bot():
     return bot
