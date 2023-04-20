84e1980c7dbd8950e9c63422a2bccf652138a6f7
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 11:09:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def main():
     basic.run_basic()
     eventasyncio.run_eventasyncio()
     bot = get_bot()
-    await bot.start(TOKEN)
+    await bot.run(TOKEN)
 
 if __name__ == '__main__':
     asyncio.run(main())
