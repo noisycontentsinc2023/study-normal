183767e71cc4378b1432c8aff4280b3164aeb074
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 10:33:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import basic
 import eventasyncio
 import asyncio
+from bot import get_bot
 
 async def main():
     basic.run_basic()
-    bot = eventasyncio.run_eventasyncio()
+    eventasyncio.run_eventasyncio()
+    bot = get_bot()
     await bot.start("TOKEN")
 
 if __name__ == '__main__':
