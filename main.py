93df073491a47576fb2a5d5683e4c671da536982
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 11:21:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from bot import get_bot
 
 TOKEN = os.environ['TOKEN']
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.typing = False
+intents.presences = False
+
 def main():
     basic.run_basic()
     eventasyncio.run_eventasyncio()
