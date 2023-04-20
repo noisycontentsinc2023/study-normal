3371403f9f26affe9bfab2f56b7df542502719a6
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 11:40:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import discord
 from bot import get_bot
 
 TOKEN = os.environ['TOKEN']
+PREFIX = os.environ['PREFIX']
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-intents.typing = False
-intents.presences = False
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-async def main():
-    basic.run_basic()
-    eventasyncio.run_eventasyncio()
-    bot = get_bot(intents=intents)
-    await bot.start(TOKEN)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    bot.load_extension('basic')
+    bot.load_extension('eventasyncio')
+    bot.run(TOKEN)
