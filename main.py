bacb6098796973680a1ac39a711f414476d1e9bd
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-20 11:52:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import basic
-import eventasyncio
 import asyncio
-import os
-from bot import get_bot
+from basic import run_basic
+from eventasyncio import run_eventasyncio
 
 TOKEN = os.environ['TOKEN']
 
 async def main():
-    bot = get_bot()
-
-    basic.setup(bot)
-    eventasyncio.setup(bot)
-
-    await bot.start(TOKEN)
+    bot = run_basic()
+    await bot.login(TOKEN)
+    await run_eventasyncio()
+    await bot.connect()
 
 if __name__ == '__main__':
     asyncio.run(main())
